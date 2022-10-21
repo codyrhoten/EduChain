6bb6750a01fb32fed6e88dbcd3b67ac15d3a405e
Cody Rhoten <rhoten_cody@yahoo.com>
2022-10-20 21:27:18 -0400

    pass blockchain data through props

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Wallet from './components/wallet/Wallet';
+// dummy data
+import blockchain from './blockchain.json';
+import { useEffect, useState } from 'react';
 
 function App() {
+    const [blocks, setBlocks] = useState([]);
+    const [txs, setTxs] = useState([]);
+
+    useEffect(() => {
+        setBlocks(blockchain.chain);
+        let _txs = [];
+        blocks.forEach(b => _txs.push(b._txs));
+        setTxs(_txs);
+    }, [blockchain])
+
     return (
         <div className='App'>
             <Routes>
-                <Route path='/' element={<Home />} />
+                <Route 
+                    path='/' 
+                    element={<Home blocks={blocks} txs={txs} />} 
+                />
                 <Route
                     path='/wallet'
                     element={<Wallet />}
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,12 +4,10 @@ import { Container, Row } from 'react-bootstrap';
 import SearchBar from "../components/SearchBar";
 import LatestBlocks from '../components/explorer/LatestBlocks';
 import LatestTxs from '../components/explorer/LatestTxs';
-// dummy data
-import blockchain from '../blockchain.json';
 
-function Home() {
-    const [blocks, setBlocks] = useState([]);
-    const [txs, setTxs] = useState([]);
+function Home({ blocks, txs }) {
+    const [latestBlx, setLatestBlx] = useState([]);
+    const [latestTxs, setLatestTxs] = useState([]);
 
     const navLinks = [
         { name: 'Wallet', url: '/wallet' },
@@ -17,19 +15,12 @@ function Home() {
         { name: 'Miner', url: '/miner' },
     ];
 
+
+
     useEffect(() => {
-        let latestBlocks = blockchain.chain.reverse().slice(0, 5);
-        let latestTxs = [];
-
-        latestBlocks.forEach(block => {
-            if (latestTxs.length > 4) return;
-            latestTxs.push(...block.txs.reverse());
-        });
-
-        latestTxs = latestTxs.slice(0, 5);
-        setBlocks(latestBlocks);
-        setTxs(latestTxs);
-    }, []);
+        setLatestBlx(blocks.reverse().slice(0, 5));
+        setLatestTxs(txs.reverse().slice(0, 5));
+    }, [blocks, txs]);
 
     return (
         <>
@@ -37,8 +28,8 @@ function Home() {
             <Container className='postion-relative'>
                 <SearchBar />
                 <Row>
-                    <LatestBlocks blocks={blocks} />
-                    <LatestTxs txs={txs} />
+                    <LatestBlocks blocks={latestBlx} />
+                    <LatestTxs txs={latestTxs} />
                 </Row>
             </Container>
         </>
