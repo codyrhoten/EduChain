a25c9b8a4bd23de4dcb240673f5eec74e26ab9e9
Cody Rhoten <rhoten_cody@yahoo.com>
2023-04-19 21:49:19 +0000

    Header: change wallet link to a drop down menu

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,69 +1,93 @@
-import { useWallet } from '../../wallet-context';
-import { Navbar, Nav, Container, Col } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import chainImage from '../../assets/chain.webp';
-import styles from './Header.module.css';
+import { useWallet } from "../../wallet-context";
+import { Navbar, Nav, Container, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import chainImage from "../../assets/chain.webp";
+import styles from "./Header.module.css";
 
 const Header = ({ navLinks }) => {
-    const { isLocked, changeWalletState } = useWallet();
+  const { isLocked, changeWalletState } = useWallet();
 
-    function closeWallet() {
-        sessionStorage.removeItem('privKey');
-        sessionStorage.removeItem('pubKey');
-        sessionStorage.removeItem('address');
-        changeWalletState(true);
-        window.location.replace('http://localhost:9999/wallet/home');
-    }
+  function closeWallet() {
+    sessionStorage.removeItem("privKey");
+    sessionStorage.removeItem("pubKey");
+    sessionStorage.removeItem("address");
+    changeWalletState(true);
+    window.location.replace("http://localhost:9999/wallet/home");
+  }
 
-    function closeWalletButton(name) {
-        return (
-            <button
-                key='4'
-                className='mx-2 rounded border border-1 border-dark px-2'
-                style={{
-                    textDecoration: 'none',
-                    color: 'black',
-                    backgroundColor: 'rgb(255, 223, 0)',
-                }}
-                onClick={closeWallet}
-            >
-                {name}
-            </button>
-        );
-    }
-
+  function closeWalletButton(name) {
     return (
-        <Navbar>
-            <Container style={{
-                backgroundColor: 'rgb(95,158,160)',
-                borderRadius: '5px'
-            }}>
-                <Navbar.Brand as={Link} to='/' style={{ fontFamily: 'Fragment Mono' }}>
-                    <div>
-                        <img src={chainImage} height='40' alt='chain' />
-                        <span><b>EduChain</b></span>
-                    </div>
-                </Navbar.Brand>
-                <Navbar.Collapse id='navbarSupportedContent'>
-                    <Nav className='ms-auto'>
-                        {navLinks !== undefined && navLinks.map((link, i) => {
-                            if (link.name === 'Close Wallet') {
-                                return closeWalletButton(link.name);
-                            }
+      <button
+        key="4"
+        className="mx-2 rounded border border-1 border-dark px-2"
+        style={{
+          textDecoration: "none",
+          color: "black",
+          backgroundColor: "rgb(255, 223, 0)",
+        }}
+        onClick={closeWallet}
+      >
+        {name}
+      </button>
+    );
+  }
 
-                            return (
-                                <Nav.Item key={i.toString()}>
-                                    <Link to={link.url} className={styles._link}>
-                                        {link.name}
-                                    </Link>
-                                </Nav.Item>
-                            );
-                        })}
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    )
+  return (
+    <Navbar
+      fixed="top"
+      style={{
+        backgroundColor: "rgb(95,158,160)",
+      }}
+    >
+      <Navbar.Brand as={Link} to="/" style={{ fontFamily: "Fragment Mono" }}>
+        <div>
+          <img src={chainImage} height="40" alt="chain" />
+          <span>
+            <b>EduChain</b>
+          </span>
+        </div>
+      </Navbar.Brand>
+      {/* <Navbar.Collapse id="navbarSupportedContent">
+        <Nav className="ms-auto">
+          {navLinks !== undefined &&
+            navLinks.map((link, i) => {
+              if (link.name === "Close Wallet") {
+                return closeWalletButton(link.name);
+              }
+
+              return (
+                <Nav.Item key={i.toString()}>
+                  <Link to={link.url} className={styles._link}>
+                    {link.name}
+                  </Link>
+                </Nav.Item>
+              );
+            })}
+        </Nav>
+      </Navbar.Collapse> */}
+      <Nav>
+        <Nav.Item>
+          <Link to="/" className={styles._link}>
+            Explorer
+          </Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Link to="/faucet" className={styles._link}>
+            Faucet
+          </Link>
+        </Nav.Item>
+        <Nav.Item>
+          <div className='dropdown dropdown-toggle'>
+            <p className={styles._link} data-bs-toggle="dropdown" aria-expanded="false">Wallet</p>
+            <ul className='dropdown-menu'>
+              <li><Link to='/wallet/home' className={styles._link}>Create</Link></li>
+              <li><Link to='/wallet/open' className={styles._link}>Open</Link></li>
+            </ul>
+          </div>
+        </Nav.Item>
+      </Nav>
+    </Navbar>
+  );
 };
 
 export default Header;
--- client/src/pages/explorer/ExplorerHome.js
+++ client/src/pages/explorer/ExplorerHome.js
@@ -1,21 +1,25 @@
-import Header from '../../components/Header/Header';
-import { Container, Row } from 'react-bootstrap';
-import LatestBlocks from '../../components/explorer/LatestBlocks';
-import LatestTxs from '../../components/explorer/LatestTxs';
+import Header from "../../components/Header/Header";
+import { Container, Row } from "react-bootstrap";
+import LatestBlocks from "../../components/explorer/LatestBlocks";
+import LatestTxs from "../../components/explorer/LatestTxs";
 
 function ExplorerHome({ latestBlx, latestTxs, navLinks }) {
-    return (
-        <>
-            <Header navLinks={navLinks} />
-            <h4 className='text-center my-4'><i>EduChain PoW Testnet Explorer</i></h4>
-            <Container className='postion-relative'>
-                <Row>
-                    <LatestBlocks latestBlx={latestBlx} />
-                    <LatestTxs latestTxs={latestTxs} />
-                </Row>
-            </Container>
-        </>
-    );
+  return (
+    <>
+      <Header navLinks={navLinks} />
+      <Container style={{ marginTop: "150px" }}>
+        <h4 className="text-center my-4">
+          <i>EduChain PoW Testnet Explorer</i>
+        </h4>
+        <Container className="postion-relative">
+          <Row>
+            <LatestBlocks latestBlx={latestBlx} />
+            <LatestTxs latestTxs={latestTxs} />
+          </Row>
+        </Container>
+      </Container>
+    </>
+  );
 }
 
 export default ExplorerHome;
